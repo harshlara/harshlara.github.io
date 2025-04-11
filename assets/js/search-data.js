70c10f4062c492fd2b9ddf325327f38d8dd5ecb0
harshlara <harshlara10@gmail.com>
2025-04-11 18:41:45 +0000

    Deploying to gh-pages from @ harshlara/harshlara.github.io@848dd7811c6cda8b1c70dc7de3b49a4a34c5d039 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -28,7 +28,7 @@ ninja.data = [{
           description: "",
           section: "Navigation",
           handler: () => {
-            window.location.href = "/_pages/goals.html";
+            window.location.href = "/goals/";
           },
         },{
         id: 'social-facebook',
