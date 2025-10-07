54b571ad70e4c5711d4f93e8a1291d181d2cce08
harshlara <harsh@resolve.ai>
2025-10-07 00:58:09 +0000

    Deploying to gh-pages from @ harshlara/harshlara.github.io@7b3987d3e2bbabc7d75a4fbd43dc6016ced82007 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -23,8 +23,8 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/cv/";
           },
-        },{id: "nav-goals",
-          title: "goals",
+        },{id: "nav-compass",
+          title: "compass",
           description: "",
           section: "Navigation",
           handler: () => {
