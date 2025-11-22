e3ddfc229a341960f223ba7b3fc7020be1550955
harshlara <harsh@resolve.ai>
2025-11-22 09:08:59 +0000

    Deploying to gh-pages from @ harshlara/harshlara.github.io@e093d70bf1687f8391278ad616b9b3b3d2c9538b 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -18,7 +18,7 @@ ninja.data = [{
           },
         },{id: "nav-cv",
           title: "CV",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+          description: "A visual representation of my career journey across AI research and engineering (generated using Nano Banana Pro).",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
